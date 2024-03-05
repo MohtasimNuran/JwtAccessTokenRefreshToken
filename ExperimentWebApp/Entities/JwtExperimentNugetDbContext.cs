a7d38a1ccce1998457c124894cebfee2a13d8509
mnuran-july <mnuran@julyservices.com>
2024-03-05 21:24:24 +0600

    updated

--- ExperimentWebApp/Entities/JwtExperimentNugetDbContext.cs
+++ ExperimentWebApp/Entities/JwtExperimentNugetDbContext.cs
@@ -18,8 +18,7 @@ public partial class JwtExperimentNugetDbContext : DbContext
     public virtual DbSet<UserInfo> UserInfos { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-T9S3VKL;Database=JwtExperimentNugetDB;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=tcp:nuran-cse-ju.database.windows.net,1433;Initial Catalog=JwtNugetDB;Persist Security Info=False;User ID=nuran.cse.ju;Password=NuR@n....;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
